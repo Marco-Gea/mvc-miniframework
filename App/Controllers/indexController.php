79629e93a5bc50fb33655d686edabe6d6002803c
Marco Aurélio Gea Júnior <marcogea.co@gmail.com>
2020-08-06 10:33:21 -0300

    Creating views.

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -5,12 +5,27 @@ namespace App\Controllers;
 //The IndexController's actions
 class IndexController {
 
+    private $view;
+
+    public function __construct(){
+        $this->view = new \stdClass();
+    }
+
     public function index(){
-        echo "We got IndexController and index function";
+        $this->view->data = array('Sofa', 'Chair', 'Bed');
+        $this->render('index');
     }
 
     public function aboutus(){
-        echo "We got IndexController and aboutus function";
+        $this->view->data = array('Notebook', 'Microwave', 'Table');
+        $this->render('aboutus');
+    }
+
+    public function render($view){
+        $class = get_class($this);
+        $class = str_replace('App\\Controllers\\', '', $class);
+        $class = strtolower(str_replace('Controller', '', $class));
+        require_once "../App/Views/".$class."/".$view.".phtml";
     }
 }
 
